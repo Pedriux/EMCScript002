905341410899d71abe3ee781f6804fb3b9b807c5
Pedriux <pedriux-@hotmail.com>
2020-06-17 00:25:37 -0500

    empecemos con ECMSript

---
+++ 01.-var-let-const.js
@@ -0,0 +1,10 @@
+console.log("### VAR, LET, CONST ###");
+
+// Declaración de variables
+
+var nombre_var = "Peter";
+if (nombre_var === "Peter") {
+    nombre_var = "Juanssss";
+}
+
+console.log("nombre_var " + nombre_var);
